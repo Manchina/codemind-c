f561e32556bb85963cd243a0aec6a3222ff28f66
Manchina <104674915+Manchina@users.noreply.github.com>
2022-07-16 17:26:59 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     for(int i=n;i>=1;i--)     {         for(int j=1;j<=i-1;j++)         {             printf(" ");         }         for(int k=1;k<=n;k++)         {            if(i==1 || i==n || k==1 || k==n)             printf("*");             else             printf(" ");         }         printf(" ");     }     return 0; }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,16 +1,23 @@
 #include<stdio.h>
-int main()
-{
-    int t,i,j;
-    scanf("%d",&t);
-    for(i=1;i<=t;i++)
-    {
-        for(j=1;j<=i;j++)
-        {
-            printf("%d",j);
-        }
+int main()  
+{  
+    int n;  
+    scanf("%d",&n);  
+    for(int i=n;i>=1;i--)  
+    {  
+        for(int j=1;j<=i-1;j++)  
+        {  
+            printf(" ");  
+        }  
+        for(int k=1;k<=n;k++)  
+        {  
+           if(i==1 || i==n || k==1 || k==n)  
+            printf("*");  
+            else  
+            printf(" ");   
+        }  
         printf("
-");
-    }
-    
+");  
+    }  
+    return 0;  
 }
