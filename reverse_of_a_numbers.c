1f736ef7301ccd321449b01ea63b08a878173e22
Manchina <104674915+Manchina@users.noreply.github.com>
2022-08-31 09:51:21 +0530

    #include<stdio.h> int main() {     int n,rev=0,rem;     scanf("%d",&n);     while(n!=0)     {         rem=n%10;         rev=rev*10+rem;         n=n/10;     }     printf("%d",rev);     return 0; }

--- reverse_of_a_numbers.c
+++ reverse_of_a_numbers.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int sum=0,temp,n;
+    int n,rev=0,rem;
     scanf("%d",&n);
-    while(n>0)
+    while(n!=0)
     {
-        temp=n%10;
-        sum=sum*10+temp;
+        rem=n%10;
+        rev=rev*10+rem;
         n=n/10;
     }
-    printf("%d",sum);
+    printf("%d",rev);
     return 0;
 }
