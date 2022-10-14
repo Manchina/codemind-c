07a5fc97999a297d400a501c4ebcf4de59c0aec3
Manchina <104674915+Manchina@users.noreply.github.com>
2022-10-14 20:23:48 +0530

    #include<stdio.h> int main(){     int n,temp,i,dope,flag=0;     scanf("%d",&n);     temp = n/2;     for(i=0; i<temp; i++)     {         dope = i * i;         if(dope == n){             flag=1;             break;         }     }     if(flag == 0){         printf("False");     }     else     {         printf("True");     } }

--- perfect_square_root_or_not.c
+++ perfect_square_root_or_not.c
@@ -1,18 +1,21 @@
 #include<stdio.h>
-#include<math.h>
-int main()
-{
-    int i,n;
-    float v;
+int main(){
+    int n,temp,i,dope,flag=0;
     scanf("%d",&n);
-    v=sqrt(n);
-    i=v;
-    if(v==i)
+    temp = n/2;
+    for(i=0; i<temp; i++)
     {
-        printf("True");
+        dope = i * i;
+        if(dope == n){
+            flag=1;
+            break;
+        }
+    }
+    if(flag == 0){
+        printf("False");
     }
     else
     {
-        printf("False");
+        printf("True");
     }
 }
